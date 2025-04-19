c0bbf28ce8ff5085857f5a53666f2d19b96f8fee
Rodogoy <74281745+Rodogoy@users.noreply.github.com>
2025-04-19 08:51:46 +0300

    Update main.cpp

--- manager/main.cpp
+++ manager/main.cpp
@@ -4,13 +4,12 @@
 #include "main.hpp"
 #include "httplib.h"
 
-extern std::shared_ptr<TaskQueue> taskQueue = std::make_shared<TaskQueue>();
-extern std::shared_ptr<TaskStorage> GlobalTaskStorage = std::make_shared<TaskStorage>();
-extern std::shared_ptr<RoundRobin> LoadBalancer = std::make_shared<RoundRobin>();
+std::shared_ptr<TaskQueue> taskQueue = std::make_shared<TaskQueue>();
+std::shared_ptr<TaskStorage> GlobalTaskStorage = std::make_shared<TaskStorage>();
+std::shared_ptr<RoundRobin> LoadBalancer = std::make_shared<RoundRobin>();
+std::shared_ptr<TaskDispatcher> dispatcher = std::make_shared<TaskDispatcher>(taskQueue);
 
 int main() {
-
-    dispatcher = std::make_shared<TaskDispatcher>(taskQueue);
     dispatcher->Start();
 
     Start(taskQueue, dispatcher);
